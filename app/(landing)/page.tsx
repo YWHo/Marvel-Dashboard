83b8af7e56f82174ea092c0766bf52a51d5c13c1
Brian Ho <YWHo@users.noreply.github.com>
2024-09-14 00:23:04 +1200

    add landing, dashboard, and details page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)] p-8">
+      <h1>Landing page</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ app/details/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DetailsPage() {
+    return (
+        <div className="font-[family-name:var(--font-geist-sans)] p-8">
+          <h1>Details page</h1>
+        </div>
+      );
+}
+
+export default DetailsPage;
