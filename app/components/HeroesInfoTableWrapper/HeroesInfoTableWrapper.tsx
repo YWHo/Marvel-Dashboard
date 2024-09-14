bbbc18b72fe769f46ef6bcdd0efd4d1d51717b10
Brian Ho <YWHo@users.noreply.github.com>
2024-09-15 04:12:07 +1200

    update cursor-pointer to happens at rows, not at full table

--- app/components/HeroesInfoTableWrapper/HeroesInfoTableWrapper.tsx
+++ app/components/HeroesInfoTableWrapper/HeroesInfoTableWrapper.tsx
@@ -26,7 +26,7 @@ const characterList: InfoList = marvelCharacters.results.map((item) => ({
  */
 export function HeroesInfoTableWrapper() {
   const router = useRouter();
-  return (<InfoTable className="cursor-pointer" list={characterList} dataType={MarvelDataType.WITH_IMAGE} onClickCallback={(id: number) => {
+  return (<InfoTable list={characterList} dataType={MarvelDataType.WITH_IMAGE} onClickCallback={(id: number) => {
     router.push(`/heroes/${id}`);
   }}/>)
 }
--- app/components/InfoTable/InfoTable.tsx
+++ app/components/InfoTable/InfoTable.tsx
@@ -17,12 +17,12 @@ export function InfoTable({ className, dataType, list, onClickCallback }: Props)
   return (
     <div className={clsx("min-[100px]: max-w-screen-lg", className)}>
       <p className="font-bold mb-2 text-white">InfoTable</p>
-      <ul className="flex-grow max-h-[calc(100vh-180px)] flex flex-col list-none p-0 overflow-y-auto gap-y-1">
+      <ul className="flex-grow max-h-[calc(100vh-180px)] flex flex-col list-none p-0 overflow-y-auto gap-y-2">
         {dataType == MarvelDataType.WITH_IMAGE && list.map((item) => (
-          <RowComponentWithImage key={item.id} {...item} onClickCallback={onClickCallback} />
+          <RowComponentWithImage className="cursor-pointer" key={item.id} {...item} onClickCallback={onClickCallback} />
         ))}
         {dataType == MarvelDataType.SIMPLE && list.map((item, i) => (
-          <RowComponentSimple key={`${i}_${item.title.substring(5)}`} {...item} />
+          <RowComponentSimple className="cursor-pointer" key={`${i}_${item.title.substring(5)}`} {...item} />
         ))}
       </ul>
     </div>
