905c8204ec9f5b07ece3ed318aec0ee3a072e627
Brian Ho <YWHo@users.noreply.github.com>
2024-09-16 08:07:51 +1200

    update the styling on Navbar to resolve overlapping on Navbar issue"

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <section className="fixed bottom-0 left-0 w-full p-6 h-10 flex justify-end items-center bg-blue-900 border-t-2 border-gray-700">
+    <section className="fixed bottom-0 left-0 w-full p-6 h-10 flex justify-end items-center bg-blue-900 border-t-2 border-gray-700 z-10">
       <Link href="https://www.marvel.com/">
         <p className="text-green-600 text-sm underline mr-10">{`Data provided by Marvel. © 2014 Marvel`}</p>
       </Link>
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,30 +3,30 @@ import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full py-1 px-4 bg-blue-900 text-white p-1 border-b-2 border-gray-500">
+    <nav className="fixed top-0 left-0 w-full py-1 px-4 bg-blue-900 text-white p-1 border-b-2 border-gray-500 z-50">
       <div className="flex items-center justify-between">
         {/* Left-aligned "Home" link */}
         <div>
           <Link
             href="/heroes"
-            className="text-xl font-semibold hover:text-gray-300 hover:underline"
+            className="text-sm md:text-xl font-semibold hover:text-gray-300 hover:underline"
           >
             Home
           </Link>
         </div>
 
         {/* Centered links */}
-        <div className="flex-1 flex justify-center space-x-6">
-          <a href="#comic_group" className="text-lg hover:text-gray-300 hover:underline">
+        <div className="flex-1 flex justify-center pl-4 space-x-1 md:space-x-6">
+          <a href="#comic_group" className="text-sm md:text-lg hover:text-gray-300 hover:underline">
             Comics
           </a>
-          <a href="#event_group" className="text-lg hover:text-gray-300 hover:underline">
+          <a href="#event_group" className="text-sm md:text-lg hover:text-gray-300 hover:underline">
             Events
           </a>
-          <a href="#series_group" className="text-lg hover:text-gray-300 hover:underline">
+          <a href="#series_group" className="text-sm md:text-lg hover:text-gray-300 hover:underline">
             Series
           </a>
-          <a href="#story_group" className="text-lg hover:text-gray-300 hover:underline">
+          <a href="#story_group" className="text-sm md:text-lg hover:text-gray-300 hover:underline">
             Stories
           </a>
         </div>
